ca0ef49045227983f09ae06b40bfb4abbc1eacab
ben hong <hyh014@ucsd.edu>
2020-09-05 17:14:56 -0500

    deleting some comments

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -55,50 +55,6 @@ export class SearchComponent implements OnInit {
 
 
   ngOnInit(): void {
-    // //google
-    // axios({
-    //   method: 'get',
-    //   url: 'https://www.googleapis.com/customsearch/v1?key=AIzaSyB2Yz5IdLNSmZ8bCCjl-A282wmoAwXxLg4&cx=daec484231fc3acee&q=lecture',
-    // }).then((res) => {
-    //   console.log(res);
-    // }).catch((err) => {
-    //   console.log(err);
-    // })
-    // //ticket master
-    // axios({
-    //   method:'get',
-
-    //   url:'https://app.ticketmaster.com/discovery/v2/events.json?apikey=f5X2atDzgbX2BdWhrPA9ZM5Q8yTYXLLk'
-    // }).then((res)=>{
-    //   console.log(res);
-    // }).catch((err)=>{
-    //   console.log(err);
-    // })
-    //wikipedia
-    // axios({
-    //   method:'get',
-    //   // headers: {
-    //   //   'Access-Control-Allow-Origin' : '*',
-    //   //   'Content-Type': 'application/json'
-    //   // },
-    //   url:'http://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search=pizza&limit=1&namespace=0'
-    // }).then((res)=>{
-    //   console.log(res);
-    // }).catch((err)=>{
-    //   console.log(err);
-    // })
-    // axios({
-    //   method:'get',
-    //   // headers: {
-    //   //   'Access-Control-Allow-Origin' : '*',
-    //   //   'Content-Type': 'application/json'
-    //   // },
-    //   url:'https://en.wikipedia.org/w/api.php?origin=*&action=parse&format=json&page=house&prop=wikitext&section=0&disabletoc=1'
-    // }).then((res)=>{
-    //   console.log(res);
-    // }).catch((err)=>{
-    //   console.log(err);
-    // })
   }
 
 }
